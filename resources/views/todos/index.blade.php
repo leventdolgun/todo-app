83cdbe5f5f1b561b0f1e9a6afd91e0a2a522b8d9
leventdolgun <dolgunlevent97@gmail.com>
2024-05-12 09:48:45 +0300

    add_todo, delete_todo, update_todo modules have been completed

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,18 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Document</title>
 </head>
+
 <body>
-<ul>
-    @foreach ( $todos as $todo )
-    <li>
-        {{$todo}}
-    </li>
-    @endforeach
-</ul>
+
+
+    @if (session()->has('notification'))
+        <div style="background-color: red; color: white">
+            {{ session()->get('notification') }}
+        </div>
+    @endif
+
+
+    <form action="/" method="post" class="mb-6">
+        @csrf
+        <input type="text" name="yapilacak_is" id="value">
+        <button class="/">ekle</button>
+
+    </form>
+    <ul>
+        @foreach ($todos as $todo)
+            <li>
+                <form method="post" action="/{{ $todo->id }}">
+                    @csrf
+                    @method('PUT')
+
+                    <input type="text" name="yapilacak_is" value="{{ $todo->value }}">
+                    <input type="checkbox" name="tamamlandi_mi" {{ $todo->is_completed ? 'checked' : '' }}>
+
+                    {{-- {{ $todo }} --}}
+
+                    <button class="bg-yellow-600"> güncelle {{ $todo->id }}</button>
+                </form> 
+
+                <form method="post" action="/{{ $todo->id }}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="bg-red-600">sil {{ $todo->id }}</button>
+                </form>
+
+            </li>
+        @endforeach
+    </ul>
+
+
+    <button type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">todo
+        list sayfasi</button>
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::post('/', [TodoController::class, 'store']);
+Route::put('/{todo}', [TodoController::class, 'update']);
+Route::delete('/{todo}', [TodoController::class, 'destroy']);
 
